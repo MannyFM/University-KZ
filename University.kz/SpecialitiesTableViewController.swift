9105cdfae778737a5aa0d2d0d4198e57c28ca559
Manny <Alibek.manabayev@gmail.com>
2016-06-24 16:28:20 +0600

    ________

--- University.kz/SpecialitiesTableViewController.swift
+++ University.kz/SpecialitiesTableViewController.swift
@@ -37,23 +37,63 @@ class SpecialitiesTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return specialities.count
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return specialities.count
+        //0. ENT for Altin Belgi
+        //1. ENT for Norm
+        //2. ENT for Auto
+        //3. ENT for Winner
+        //4. ENT for Village
+        //5. speciality id
+        return 6
     }
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! SpecialityTableViewCell
         
-        let index = indexPath.row
-        cell.textLabel?.text = specialities[index].name
+        let index = indexPath.section
+        let spec = specialities[index]
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = "Минимальный балл ЕНТ для обладателей знака «Алтын белгі»"
+            cell.detailTextLabel?.text = "\(spec.minENTAB)"
+        case 1:
+            cell.textLabel?.text = "Минимальный балл ЕНТ для смертных"  //<<<<<<<<<<<==================================================================
+            //Лица, не являющиеся обладателями преимущественного права
+            cell.detailTextLabel?.text = "\(spec.minENTProst)"
+        case 2:
+            cell.textLabel?.text = "Минимальный балл ЕНТ для выпускников автономных организаций образования"
+            cell.detailTextLabel?.text = "\(spec.minENTAuto)"
+        case 3:
+            cell.textLabel?.text = "Минимальный балл ЕНТ для победителей международных, республиканских и президентских олимпиад, научных и спортивных соревнований"
+            cell.detailTextLabel?.text = "\(spec.minENTWinner)"
+        case 4:
+            cell.textLabel?.text = "Минимальный балл ЕНТ для обладателей сельской квоты"
+            cell.detailTextLabel?.text = "\(spec.minENTVillage)"
+        case 5:
+            cell.textLabel?.text = "ID специальности"
+            cell.detailTextLabel?.text = spec.id ?? "---"
+        default: break
+            //
+        }
         
         return cell
     }
     
+    // MARK: Header
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section \(section)"
+    }
+    
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let vw = UIView()
+        vw.backgroundColor = UIColor.whiteColor()
+        
+        return vw
+    }
     
     // MARK: - Navigation
     
